3591ce2fae73f85a279e7cf5426a5a2ed4d5d881
Ali Raza <dejavu87@gmail.com>
2021-03-16 13:45:16 +0500

    added rate limiter

--- app/adapters/__init__.py
+++ app/adapters/__init__.py
@@ -8,8 +8,20 @@ class SMS:
             r = post(
                 url=os.getenv("MONTY_BASEURL") + "/smppServers/sms",
                 headers={"Authorization": os.getenv("MONTY_AUTH_HEADER")},
-                json={"from": "Digi Alert", "to": destination, "message": message},
+                json={"from": "Digi Alert", "to": destination, "message": "This is a test message: " + message},
             )
             return r.json()
         except:
             return "error"
+
+class Email:
+    def send(self, recipients, subject, message):
+        try:
+            r = post(
+                url=os.getenv("MONTY_BASEURL") + "/email",
+                headers={"Authorization": os.getenv("MONTY_AUTH_HEADER")},
+                json={"To": recipients, "Subject": subject, "Body": message},
+            )
+            return r.json()
+        except:
+            return 'error'
--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
 from app.core.config import settings
 
-from adapters import SMS
+from adapters import SMS, Email
 
+limiter = Limiter(key_func=get_remote_address)
 
 def get_application():
     _app = FastAPI(title=settings.PROJECT_NAME)
+
+    _app.state.limiter = limiter
+    _app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
     _app.add_middleware(
         CORSMiddleware,
@@ -23,8 +30,18 @@ def get_application():
 app = get_application()
 
 sms = SMS()
+email = Email()
+
 
 @app.post("/send_sms")
-def send_sms(destination: str, message: str):
+@limiter.limit("5/minute")
+def send_sms(destination: str, message: str, request: Request):
     message_payload = {}
     return sms.send(destination, message)
+
+
+@app.post("/send_email")
+@limiter.limit("5/minute")
+def send_sms(recipient: str, subject: str, message: str, request: Request):
+    message_payload = {}
+    return email.send([recipient], subject, message)
