dc6419ae5f23cedfdbb4d250377cf3f99897b8a3
Ali Raza <dejavu87@gmail.com>
2021-03-16 14:14:19 +0500

    removed bulk and updated readme.md

--- README.md
+++ README.md
@@ -5,3 +5,23 @@ This project was generated via [manage-fastapi](https://ycd.github.io/manage-fas
 ## License
 
 This project is licensed under the terms of the MIT license.
+
+## How to run
+
+You will need a .env file to run locally. Here is a sample:
+
+```
+PROJECT_NAME=Paighaam
+BACKEND_CORS_ORIGINS=["http://localhost:8000", "https://localhost:8000", "http://localhost", "https://localhost"]
+MONTY_BASEURL=<>
+MONTY_AUTH_HEADER=<>
+```
+
+Once env is setup you can just do a ```docker-compose up```
+
+
+## Dev Roadmap (PRs welcome)
+-[] Push Notifications
+-[] Unit Tests
+-[] Better Security
+-[] Better Logging
--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 
 from typing import Any, Dict, List, Optional, Union
 
-from pydantic import AnyHttpUrl, BaseSettings, PostgresDsn, validator
+from pydantic import AnyHttpUrl, BaseSettings, validator
 
 
 
@@ -17,24 +17,6 @@ class Settings(BaseSettings):
         elif isinstance(v, (list, str)):
             return v
         raise ValueError(v)
-
-    POSTGRES_SERVER: str
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
-    DATABASE_URI: Optional[PostgresDsn] = None
-
-    @validator("DATABASE_URI", pre=True)
-    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
-        if isinstance(v, str):
-            return v
-        return PostgresDsn.build(
-            scheme="postgresql",
-            user=values.get("POSTGRES_USER"),
-            password=values.get("POSTGRES_PASSWORD"),
-            host=values.get("POSTGRES_SERVER"),
-            path=f"/{values.get('POSTGRES_DB') or ''}",
-        )
 
     class Config:
         case_sensitive = True
